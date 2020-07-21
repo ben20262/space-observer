2aba28a972c80061ebd27663ce9205523d6296e3
ben20262 <ben20262@aim.com>
2020-07-21 15:21:08 -0400

    added store and thunk for api use

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import indexReducer from './reducers/index'
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(indexReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
